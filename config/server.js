360a599508dc1def18dc73ee37b8f4086a881a36
Hamilton Garcia <hngr98@gmail.com>
2021-10-17 16:04:25 -0600

    First commit and up

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5b8452734ac38e111f9e2f31098a533e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '78fa5ad0-3137-49c3-9124-64a70833b078'
+};
